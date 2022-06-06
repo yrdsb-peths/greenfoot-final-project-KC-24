0c3c52f5fff026bc36fa461d5e0950545bcbadc1
KC-24 <102365631+KC-24@users.noreply.github.com>
2022-06-06 09:09:36 -0400

    Add files via upload
    added title screen and start button and started on instructions

---
+++ Instruction.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class instrution here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Instruction extends Actor
+{
+    /**
+     * Act - do whatever the instrution wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+    } 
+    
+    public Instruction(){
+    }
+}
---
+++ StartButton.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class startButton here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StartButton extends Actor
+{
+    /**
+     * Act - do whatever the startButton wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        if(Greenfoot.mouseClicked(this)){
+            Greenfoot.setWorld(new MyWorld());
+        }
+        
+    }    
+    
+    public StartButton(){
+        getImage().scale(220,120);
+    }
+}
